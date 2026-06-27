0d77c8adbb443eb3a2aaf845d34697811452008d
Sayyid Barqi <sayyidbarqi0911@gmail.com>
2026-06-27 18:03:32 +0700

    Fix: adjust script filename path for Laravel serverless execution

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
-// api/index.php
-require __DIR__ . '/../public/index.php';
+
+// Paksa path agar Laravel tidak kebingungan dengan routing di Vercel
+$_SERVER['SCRIPT_FILENAME'] = dirname(__DIR__) . '/public/index.php';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
+require dirname(__DIR__) . '/public/index.php';
